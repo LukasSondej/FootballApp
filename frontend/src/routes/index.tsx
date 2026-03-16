1dc9ec0ddbd988f2f4b4a44d2fb7e3e8aed28d57
Łukasz Sondej <sondejlukasz0@gmail.com>
2026-03-16 12:48:03 +0100

    removing unnecessary code

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,13 +2,10 @@ import { createFileRoute } from '@tanstack/react-router'
 import { matchesQueryOptions } from '../hooks/useGetMatches'
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent,
   loader: async({context}) => {
     const {queryClient} = context
     return queryClient.ensureQueryData(matchesQueryOptions)
   }
 })
 
-function RouteComponent() {
-  return <div>Hello "/"!</div>
-}
+
