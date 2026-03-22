d04e05bce5cc041c734544af84b64f1d3cfd27e1
Łukasz Sondej <sondejlukasz0@gmail.com>
2026-03-22 19:23:51 +0100

    update CORS origin for new domain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://dynamic-pie-266080.netlify.app' 
+    'https://football-app-sondej.netlify.app'
   ],
   credentials: true
 })); 
