19aa20622bbbebbd5a03cfae9142b7988492bc24
Łukasz Sondej <sondejlukasz0@gmail.com>
2026-04-18 21:41:50 +0200

    delete prisma adapter

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,5 +1,5 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL as string });
-const prisma = new PrismaClient({ adapter });
-export default prisma
+
+const prisma = new PrismaClient();
+
+export default prisma;
